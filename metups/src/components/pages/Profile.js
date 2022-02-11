7f08d6fbb23b04da00590dc58ea66f61064f2164
florent-haxhiu <florent_h2@hotmail.com>
2022-02-11 16:41:23 +0000

    Finished the navbar for now

--- metups/src/components/pages/Profile.js
+++ metups/src/components/pages/Profile.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Profile = () => {
-  return <div>Profile</div>
+
+  // State variables
+  const [ people, setPeople ] = useState([])
+
+  return (
+    <>
+      {/* Need an rounded Image, next to it. Display username, email and allow them to create an event */}
+      <section>
+
+      </section>
+    </>
+  )
 }
 
 export default Profile
