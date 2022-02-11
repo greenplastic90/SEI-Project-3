d65f6354c1ea74e355d7de0f1dac0d77ba3fc663
Bashar Othman <othman90@gmail.com>
2022-02-11 17:36:07 +0200

    added Auth.js in helpers folder

---
+++ metups/src/auth/helpers.js
@@ -0,0 +1,18 @@
+const getTokenFromLocalStorage = () => {
+  return window.localStorage.getItem('metups-login-token')
+}
+
+export const getPayload = () => {
+  const token = getTokenFromLocalStorage()
+  if (!token) return
+  const splitToken = token.split('.')
+  if (splitToken.length !== 3) return
+  return JSON.parse(Buffer.from(splitToken[1], 'base64'))
+}
+
+export const userIsAuthenticated = () => {
+  const payload = getPayload()
+  if (!payload) return
+  const currentTime = Math.round(Date.now() / 1000)
+  return currentTime < payload.exp
+}
