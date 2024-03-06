5fe485732381ec94df19a84fa571abc2a96aabb2
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2024-03-06 14:53:17 +0300

    Refactoed Profile and made it responsive

--- node-server/metups/src/components/pages/profile/Profile.js
+++ node-server/metups/src/components/pages/profile/Profile.js
@@ -1,18 +1,32 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { getTokenFromLocalStorage } from '../../../auth/helpers'
-import { Box, Heading, Stack, VStack, useDisclosure } from '@chakra-ui/react'
+import {
+  Box,
+  Grid,
+  HStack,
+  Heading,
+  Stack,
+  Text,
+  VStack,
+  useBreakpoint,
+  useDisclosure,
+} from '@chakra-ui/react'
 import BurgerFooter from '../common/navbar/components/BurgerFooter'
 import ResetPasswordModal from './ResetPasswordModal'
 import { useNavigate } from 'react-router-dom'
-import EventCards from '../common/EventCards'
 import ProfileImage from './ProfileImage'
 import EventsCounter from './EventsCounter'
+import SmallEventCard from '../common/event-cards/SmallEventCard'
+import LargeEventCard from '../common/event-cards/LargeEventCard'
+
+const smallCardBreakpoints = ['base', 'sm']
 
 function Profile({ user, setUser, handleLogout }) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [eventsToDisplay, setEventsToDisplay] = useState({ title: '', events: [] })
   const navigate = useNavigate()
+  const breakpoint = useBreakpoint()
 
   function logoutAndNavigate() {
     handleLogout()
@@ -41,6 +55,7 @@ function Profile({ user, setUser, handleLogout }) {
       console.log(error)
     }
   }, [navigate, setUser])
+
   function eventsSwitch(title) {
     switch (title) {
       case 'My Events':
@@ -52,32 +67,36 @@ function Profile({ user, setUser, handleLogout }) {
       default:
     }
   }
+
   return (
     <VStack spacing={6}>
       {user && (
         <>
-          <VStack pb={4} justify={'space-between'} bgColor={'gray.100'} borderRadius={'xl'}>
-            <ProfileImage user={user} />
-            <EventsCounter user={user} action={eventsSwitch} />
-          </VStack>
-
-          {eventsToDisplay.title && (
-            // 350 is the width of the VStack above
-            <Stack borderRadius={'xl'} maxW={'350'}>
-              <Heading>{eventsToDisplay.title}</Heading>
-              <EventCards events={eventsToDisplay.events} />
+          {!smallCardBreakpoints.includes(breakpoint) ? (
+            <HStack w={'full'} justify={'space-around'} align={'start'} spacing={6}>
+              <ImageAndEvents
+                user={user}
+                eventsSwitch={eventsSwitch}
+                eventsToDisplay={eventsToDisplay}
+                onOpen={onOpen}
+                logoutAndNavigate={logoutAndNavigate}
+                isOpen={isOpen}
+                onClose={onClose}
+              />
+            </HStack>
+          ) : (
+            <Stack>
+              <ImageAndEvents
+                user={user}
+                eventsSwitch={eventsSwitch}
+                eventsToDisplay={eventsToDisplay}
+                onOpen={onOpen}
+                logoutAndNavigate={logoutAndNavigate}
+                isOpen={isOpen}
+                onClose={onClose}
+              />
             </Stack>
           )}
-
-          <Box p={4} w={'350px'} borderRadius={'xl'} bgColor={'gray.100'}>
-            <BurgerFooter
-              user={user}
-              action={'Password reset'}
-              actionFunc={onOpen}
-              handleLogout={logoutAndNavigate}
-            />
-          </Box>
-          <ResetPasswordModal isOpen={isOpen} onClose={onClose} />
         </>
       )}
     </VStack>
@@ -85,3 +104,97 @@ function Profile({ user, setUser, handleLogout }) {
 }
 
 export default Profile
+
+function ImageAndEvents({
+  user,
+  eventsSwitch,
+  eventsToDisplay,
+  onOpen,
+  logoutAndNavigate,
+  isOpen,
+  onClose,
+}) {
+  const breakpoint = useBreakpoint()
+  console.log(breakpoint)
+  return (
+    <>
+      <Stack>
+        <ImageAndCounter user={user} eventsSwitch={eventsSwitch} />
+        {/* show ProfileFooter below ImageAndCounter if Eevnts are going to be right of ImageAndCounter */}
+        {!smallCardBreakpoints.includes(breakpoint) && (
+          <ProfileFooter
+            user={user}
+            onOpen={onOpen}
+            logoutAndNavigate={logoutAndNavigate}
+            isOpen={isOpen}
+            onClose={onClose}
+          />
+        )}
+      </Stack>
+
+      {eventsToDisplay.title && (
+        // 350 is the width of the VStack above
+        <Stack borderRadius={'xl'} maxW={'350'}>
+          <Heading>{eventsToDisplay.title}</Heading>
+          {eventsToDisplay.events.length !== 0 ? (
+            smallCardBreakpoints.includes(breakpoint) ? (
+              <SmallEventCard events={eventsToDisplay.events} />
+            ) : (
+              <Grid
+                templateColumns={[
+                  null,
+                  null,
+                  null,
+                  'repeat(1, 1fr)',
+                  'repeat(2, 1fr)',
+                  null,
+                  'repeat(3, 1fr)',
+                ]}
+                gap={4}>
+                <LargeEventCard events={eventsToDisplay.events} />
+              </Grid>
+            )
+          ) : (
+            <VStack>
+              <Text>No Events</Text>
+            </VStack>
+          )}
+        </Stack>
+      )}
+
+      {/* show ProfileFooter below Events if Eevnts are going to be below ImageAndCounter */}
+      {smallCardBreakpoints.includes(breakpoint) && (
+        <ProfileFooter
+          user={user}
+          onOpen={onOpen}
+          logoutAndNavigate={logoutAndNavigate}
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      )}
+    </>
+  )
+}
+function ImageAndCounter({ user, eventsSwitch }) {
+  return (
+    <VStack pb={4} justify={'space-between'} bgColor={'gray.100'} borderRadius={'xl'}>
+      <ProfileImage user={user} />
+      <EventsCounter user={user} action={eventsSwitch} />
+    </VStack>
+  )
+}
+function ProfileFooter({ user, onOpen, logoutAndNavigate, isOpen, onClose }) {
+  return (
+    <>
+      <Box p={4} w={'350px'} borderRadius={'xl'} bgColor={'gray.100'}>
+        <BurgerFooter
+          user={user}
+          action={'Password reset'}
+          actionFunc={onOpen}
+          handleLogout={logoutAndNavigate}
+        />
+      </Box>
+      <ResetPasswordModal isOpen={isOpen} onClose={onClose} />
+    </>
+  )
+}
