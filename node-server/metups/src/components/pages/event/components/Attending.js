ef8dc0bb87b008fadda90f113611e78423b6bfcf
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2024-03-11 12:05:30 +0300

    added to call to acction to Attending when no one is attending

--- node-server/metups/src/components/pages/event/components/Attending.js
+++ node-server/metups/src/components/pages/event/components/Attending.js
@@ -5,23 +5,29 @@ function Attending({ attending }) {
   return (
     <Stack>
       <Heading fontSize={'xl'} fontWeight={'bold'}>{`Attending (${attending.length})`}</Heading>
-      <HStack overflowX='auto' width='100%' minWidth='0' spacing={4} p={4}>
-        {attending.map((user) => (
-          <VStack
-            key={user._id}
-            borderRadius='md'
-            justify={''}
-            px={4}
-            pt={6}
-            pb={10}
-            boxShadow={'0px 0px 5px 0px rgba(0, 0, 0, 0.12)'}>
-            <Avatar size='lg' name={user.owner.username} src={user.owner.profilePhoto} />
-            <Text fontWeight={'bold'} fontSize={'sm'}>
-              {user.owner.username}
-            </Text>
-          </VStack>
-        ))}
-      </HStack>
+      {attending.length !== 0 ? (
+        <HStack overflowX='auto' width='100%' minWidth='0' spacing={4} p={4}>
+          {attending.map((user) => (
+            <VStack
+              key={user._id}
+              borderRadius='md'
+              justify={''}
+              px={4}
+              pt={6}
+              pb={10}
+              boxShadow={'0px 0px 5px 0px rgba(0, 0, 0, 0.12)'}>
+              <Avatar size='lg' name={user.owner.username} src={user.owner.profilePhoto} />
+              <Text fontWeight={'bold'} fontSize={'sm'}>
+                {user.owner.username}
+              </Text>
+            </VStack>
+          ))}
+        </HStack>
+      ) : (
+        <VStack h={'100px'} justify={'center'}>
+          <Text fontWeight={'bold'}>Be the first to RSVP</Text>
+        </VStack>
+      )}
     </Stack>
   )
 }
